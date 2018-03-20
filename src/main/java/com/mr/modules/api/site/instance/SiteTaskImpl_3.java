ff708b2a44fcc0d76ff874d5ee81b65a9848772c
fj168104 <fengj@jxltech.com>
2018-03-20 23:29:33 +0800

    demo

--- src/main/java/com/mr/modules/api/site/instance/SiteTaskImpl_3.java
+++ src/main/java/com/mr/modules/api/site/instance/SiteTaskImpl_3.java
@@ -40,7 +40,7 @@ public class SiteTaskImpl_3 extends SiteTaskExtend {
 		extract1(fullTxt);
 
 		//保监局处罚
-		String targetUri2 = "http://www.circ.gov.cn/web/site0/tab5241/info4102039.htm";
+		String targetUri2 = "http://www.circ.gov.cn/web/site0/tab5241/info4102424.htm";
 		fullTxt = getData(targetUri2);
 		log.info(fullTxt);
 		extract2(fullTxt);
@@ -48,7 +48,7 @@ public class SiteTaskImpl_3 extends SiteTaskExtend {
 	}
 
 	/**
-	 * 提取所需要的信息
+	 * 保监会处罚 提取所需要的信息
 	 * 序号、处罚文号、机构当事人名称、机构当事人住所、机构负责人姓名、
 	 * 当事人集合（当事人姓名、当事人身份证号、当事人职务、当事人住址）、发布机构、发布日期、行政处罚详情、处罚机关、处罚日期
 	 */
@@ -137,19 +137,19 @@ public class SiteTaskImpl_3 extends SiteTaskExtend {
 				if (pString.startsWith("依据《中华人民共和国保险法》")) {
 					isAllPersonOn = false;
 					detailIsOn = true;
-				}else {
+				} else {
 					allPerson += pString + System.getProperty("line.separator");
 				}
 
 			}
 
 			//punishDetail 处理
-			if(detailIsOn){
-				String pString = filter(pElement.text().trim(),filterTags);
-				if(pString.startsWith("中国保监会")){
+			if (detailIsOn) {
+				String pString = filter(pElement.text().trim(), filterTags);
+				if (pString.startsWith("中国保监会")) {
 					detailIsOn = false;
-					punishDetail = details.toString().replace("[","").replace("]", "");
-				}else{
+					punishDetail = details.toString().replace("[", "").replace("]", "");
+				} else {
 					details.add(pString);
 				}
 			}
@@ -166,12 +166,142 @@ public class SiteTaskImpl_3 extends SiteTaskExtend {
 	}
 
 	/**
-	 * 提取所需要的信息
-	 * 序号、处罚文号、处罚对象、处罚日期、发布机构、发布日期、名单分类、标题名称、详情
+	 * 保监局处罚 提取所需要的信息
+	 * 序号、处罚文号、机构当事人名称、机构当事人住所、机构负责人姓名、
+	 * 当事人集合（当事人姓名、当事人身份证号、当事人职务、当事人住址）、发布机构、发布日期、行政处罚详情、处罚机关、处罚日期
 	 */
-
 	private void extract2(String fullTxt) {
+		ArrayList<String> filterTags = Lists.newArrayList("<SPAN>",
+				"</SPAN>",
+				"<a>",
+				"</a>",
+				"&nbsp;",
+				"　",
+				" ",
+				" ");
 
+		//序号 TODO 需确认
+		String seqNo = "";
+		seqNo = "";
+
+		//处罚文号
+		String punishNo = "";
+		punishNo = "〔2018〕12号";    //丛链接中提取
+
+
+		boolean isPunishOn = false;
+		int pCnt = 1;
+		//机构当事人名称
+		String orgPerson = "";
+
+		//机构当事人住所
+		String orgAddress = "";
+
+		//机构负责人姓名
+		String orgHolderName = "";
+
+
+		//当事人集合
+		boolean isAllPersonOn = true;
+		String allPerson = "";
+
+		//发布机构
+		String releaseOrg = "";
+		releaseOrg = "湖北保监局";    //链接中提取
+
+		//发布日期
+		String releaseDate = "";
+		releaseDate = "2018-03-19";    //链接中提取
+
+		//行政处罚详情
+		String punishDetail = "";
+		boolean detailIsOn = false;
+		ArrayList<String> details = Lists.newArrayList();
+
+		//处罚机关	TODO 需确认
+		String punishOrg = "";
+		punishOrg = "湖北保监局";
+
+		//处罚日期
+		String punishDate = "";
+		boolean punishDateIsOn = false;
+
+		/**
+		 * 规则的当事人为公司的处理
+		 */
+		Document doc = Jsoup.parse(fullTxt);
+		Elements Ps = doc.getElementsByTag("P");
+		for (Element pElement : Ps) {
+			//orgPerson orgAddress orgHolderName 处理
+			if (Strings.isNullOrEmpty(orgPerson) || isPunishOn) {
+				Element spanElement = pElement.getElementsByTag("SPAN").get(0);
+				String pString = filter(spanElement.text().trim(), filterTags);
+				if (pString.startsWith("当事人：")) {
+					isPunishOn = !isPunishOn;
+					isAllPersonOn = !isAllPersonOn; //首次关闭当事人集合提取, 机构信息提取完成后打开
+				}
+				if (isPunishOn) {
+					if (pCnt == 1) {
+						orgPerson = pString;
+					}
+					if (pCnt == 2) {
+						orgAddress = pString;
+					}
+					if (pCnt == 3) {
+						orgHolderName = pString;
+					}
+					pCnt++;
+				}
+			}
+
+			//当事人集合
+			if (isAllPersonOn && !Strings.isNullOrEmpty(orgPerson)) {
+				Elements spanElements = pElement.getElementsByTag("SPAN");
+				for (Element spanElement : spanElements) {
+					String pString = filter(spanElement.text().trim(), filterTags);
+					if (pString.startsWith("依据《中华人民共和国保险法》")) {
+						isAllPersonOn = false;
+						detailIsOn = true;
+					}else {
+						allPerson += pString;
+					}
+					allPerson += System.getProperty("line.separator");
+				}
+
+
+
+			}
+
+			//punishDetail 处理
+			if (detailIsOn) {
+				Elements spanElements = pElement.getElementsByTag("SPAN");
+				for (Element spanElement : spanElements) {
+					String pString = filter(spanElement.text().trim(), filterTags);
+					if (pString.length() < 10 && pString.endsWith("保监局")) {
+						detailIsOn = false;
+						punishDetail = details.toString().replace("[", "").replace("]", "");
+						punishDateIsOn = true;
+						break;
+					} else {
+						details.add(pString);
+					}
+				}
+			}
+
+			//punishDate 处理
+			if (punishDateIsOn) {
+				String tmp1 = "";
+				Elements spanElements = pElement.getElementsByTag("SPAN");
+				for (Element spanElement : spanElements) {
+					tmp1 += filter(spanElement.text().trim(), filterTags);
+				}
+				if (extracterZH(tmp1).endsWith("年月日")){
+					punishDate = tmp1.substring(tmp1.indexOf("年") - 4);
+				}
+			}
+		}
+
+		log.info("parse demo...\n" + punishDetail);
 	}
 
 }
